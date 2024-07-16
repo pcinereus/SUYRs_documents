a7e61e294c355818937352ad7b0a063919a35ef6
pcinereus <pcinereus@users.noreply.github.com>
2024-07-16 23:37:17 +0000

    Deploying to gh-pages from @ pcinereus/SUYRs_documents@52c7c79657db8eef029e469ddaa84dce03783b13 🚀

---
+++ default.nix
@@ -0,0 +1,35 @@
+let
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/019f5c29c5afeb215587e17bf1ec31dc1913595b.tar.gz") {};
+ 
+  rpkgs = builtins.attrValues {
+    inherit (pkgs.rPackages) 
+      tidyverse
+      rstan
+      brms
+      emmeans
+      quarto;
+  };
+    
+  system_packages = builtins.attrValues {
+    inherit (pkgs) 
+      quarto
+      cmdstan
+      R
+      glibcLocales
+      nix;
+  };
+  
+in
+
+pkgs.mkShell {
+  LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  LANG = "en_US.UTF-8";
+   LC_ALL = "en_US.UTF-8";
+   LC_TIME = "en_US.UTF-8";
+   LC_MONETARY = "en_US.UTF-8";
+   LC_PAPER = "en_US.UTF-8";
+   LC_MEASUREMENT = "en_US.UTF-8";
+
+  buildInputs = [  rpkgs  system_packages   ];
+  
+}
