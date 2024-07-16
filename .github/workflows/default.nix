bd5b906feb13a2c3f17dc45edea8212ccd4ba187
Murray Logan <i.obesulus@gmail.com>
2024-07-16 16:55:36 +1000

    Add default nix

---
+++ .github/workflows/default.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e11142026e2cef35ea52c9205703823df225c947.tar.gz") {} }:
+
+with pkgs;
+
+let
+  my-pkgs = rWrapper.override {
+    packages = with rPackages; [ dplyr ggplot2 R];
+  };
+in
+mkShell {
+  buildInputs = [my-pkgs];
+}
