8d4534e56837b153705d88a2dbee683b7bd5105d
Murray Logan <i.obesulus@gmail.com>
2024-07-16 17:33:44 +1000

    Try again

--- default.nix
+++ default.nix
@@ -1,12 +1,34 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e11142026e2cef35ea52c9205703823df225c947.tar.gz") {} }:
-
-with pkgs;
 
 let
-  my-pkgs = rWrapper.override {
-    packages = with rPackages; [ tidyverse cmdstanr dplyr ggplot2 R];
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/019f5c29c5afeb215587e17bf1ec31dc1913595b.tar.gz") {};
+ 
+  rpkgs = builtins.attrValues {
+    inherit (pkgs.rPackages) 
+      tidyverse
+      rstan
+      brms
+      emmeans
+      cmdstanr;
   };
+    
+  system_packages = builtins.attrValues {
+    inherit (pkgs) 
+      R
+      glibcLocales
+      nix;
+  };
+  
 in
-mkShell {
-  buildInputs = [my-pkgs];
+
+pkgs.mkShell {
+  LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then  "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  LANG = "en_US.UTF-8";
+   LC_ALL = "en_US.UTF-8";
+   LC_TIME = "en_US.UTF-8";
+   LC_MONETARY = "en_US.UTF-8";
+   LC_PAPER = "en_US.UTF-8";
+   LC_MEASUREMENT = "en_US.UTF-8";
+
+  buildInputs = [  rpkgs  system_packages   ];
+  
 }
